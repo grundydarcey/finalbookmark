75c938cca8c3c56edd4bf5d0bbd959f671f9fdd9
Beyonce Knowles <you@example.com>
2020-12-03 19:03:06 -0500

    handlers fix

---
+++ scripts/handlers.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+function newBookmarkHandler() {
+  console.log('Start');
+}
+
+function expandBookmarkHandler() {
+  console.log('Start typing');
+  $('#bmk1').click(function() {
+    console.log('just got handled');
+  });
+  
+}
+
+export {newBookmarkHandler, expandBookmarkHandler };
