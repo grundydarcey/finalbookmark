e6f326be705829b8d208447399678a8e3499adef
Beyonce Knowles <you@example.com>
2020-12-03 23:47:24 -0500

    start on new bkmk

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-console */
 import store1 from './store1.js';
 import store2 from './store2.js';
 import generateErrorPage from './error.js';
 
 function generateStartPage() {
-  return `<div id="new-select">
+  return `
+  <div id="new-select">
   <div id="new" class="clicknew">
       <input type="button" id="newbk" value="Create a new bookmark">
-      <input type="button" id="oldbk" value="Get old bookmarks">
   </div>
   <div class="select" id="selected">
       <label for="stars">View bookmarks by Star Rating:</label>
@@ -19,37 +20,28 @@ function generateStartPage() {
       </select>
   </div>
 </div>
+
 <ul class="result" id="resulted">
   <li class="bmarkcontainer1" id="contained1">
-    <input type="button" id="btn" value="Open Detailed View"><br>
+  <br><input type="button" id="btn" class="clsbtnone" value="Open Detailed View"><br>
     Link Title: <a href=#mainpage>${store1.bookmarks[0].title}</a> <br>
     Star Rating: ${store1.bookmarks[0].rating}<br><br>
   </li>
   <li class="bmarkcontainer2" id="contained2">
-    <input type="button" id="btn2" value="Open Detailed View"><br>
+  <br><input type="button" id="btn2" class="clsbtntwo" value="Open Detailed View"><br>
     Link Title: ${store1.bookmarks[1].title}<br>
     Star Rating: ${store1.bookmarks[1].rating}<br><br>
   </li>
 </ul>
-`;
-}
+`;}
 
-function expandedView() {
-  return `<div id="lesgo">
-  <p>lets go</p>
-  </div>`;
-}
 
 
 function render() {
   let html = '';
   if (store1['adding'] === false) {
     html = generateStartPage();
-  } else if (store2.expanded === true) {
-    html = expandedView();
-  } else {
-    html = generateErrorPage();
-  }
+  } 
   $('#mainpage').html(html);
 }
 
@@ -59,15 +51,3 @@ function main() {
 }
 
 $(main);
-
-$(document).ready(function() {
-  $('#btn').on('click', function() {
-    console.log('start clicking');
-  });
-});
-
-$(document).ready(function() {
-  $('#btn2').on('click', function() {
-    console.log('stop making sense');
-  });
-});
